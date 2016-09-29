99be377476adcab9f7fc2a987d00f18141e8bfaa
xdoo <claus.straube@gmail.com>
2016-09-29 07:49:29 +0200

    added new resource service and token relay over rest template

--- resource_server_002/src/main/java/com/example/Application.java
+++ resource_server_002/src/main/java/com/example/Application.java
@@ -2,16 +2,11 @@ package com.example;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import java.security.Principal;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,31 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class Application {
 
-    @Autowired OAuth2RestTemplate template;
-    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Secured("ROLE_FOO_SERVICE_HELLO")
     @RequestMapping("/hello")
-    public String home() {
-        return this.calculateHello();
+    public String home(Principal user) {   
+        return this.calculateHello(user);
     }
-
+    
     @HystrixCommand(fallbackMethod = "defaultHello")
-    private String calculateHello() {
-        ResponseEntity<String> response = this.template.getForEntity("http://localhost:8071/hello", String.class);
-        return "Answer: " + response.getBody();
+    private String calculateHello(Principal user) {
+        return "Hello " + user.getName();
     }
-
-    private String defaultHello() {
-        return "No Answer: Hello World!";
+    
+    private String defaultHello(){
+        return "Hello World!";
     }
-
-    @Bean
-    public OAuth2RestTemplate restTemplate(UserInfoRestTemplateFactory factory) {
-        return factory.getUserInfoRestTemplate();
-    }
-
+    
 }
