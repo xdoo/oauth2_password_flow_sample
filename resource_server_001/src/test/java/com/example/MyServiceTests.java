be8bb468cb554f20fea1726be13b0b77b14f2e62
xdoo <claus.straube@gmail.com>
2016-10-06 13:11:28 +0200

    minor changes

--- resource_server_001/src/test/java/com/example/MyServiceTests.java
+++ resource_server_001/src/test/java/com/example/MyServiceTests.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
