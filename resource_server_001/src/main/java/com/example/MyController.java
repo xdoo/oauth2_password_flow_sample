8e6838bd6d6fccb20d99fbd6086ba73193768d3b
xdoo <claus.straube@gmail.com>
2016-11-04 16:46:12 +0100

    cleansing

--- resource_server_001/src/main/java/com/example/MyController.java
+++ resource_server_001/src/main/java/com/example/MyController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.logging.Logger;
 import javax.validation.Valid;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,10 +26,6 @@ public class MyController {
     @PreAuthorize("hasAuthority('RESOURCE_001_HELLO')")
     @RequestMapping(value = "/hello", method = RequestMethod.POST)
     public String sayHello(@Valid @RequestBody Hello hello) {
-        
-//        if(result.hasErrors()) {
-//            LOG.info("Errors in Bean!");
-//        }
         
         return this.service.sayHello(hello);
     } 
