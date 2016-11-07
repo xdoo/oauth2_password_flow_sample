f6e199d096ccce36828c8969835fa9962f208c6f
xdoo <claus.straube@gmail.com>
2016-11-07 17:35:52 +0100

    some cleansing

--- resource_server_001/src/main/java/com/example/Application.java
+++ resource_server_001/src/main/java/com/example/Application.java
@@ -2,16 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableResourceServer
-@RestController
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableAspectJAutoProxy
 public class Application {
     
     public static void main(String[] args) {
